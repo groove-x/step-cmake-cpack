5efd1672fcfeece4e44706388604e4b444c6652f
Yuma.M <yuma-m@users.noreply.github.com>
2017-11-13 18:54:21 +0900

    Fix revision

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-apt install -y devscripts fakeroot dpkg-dev
+apt-get install -y devscripts fakeroot dpkg-dev
 
-NOW=`date +'%Y%m%d-%H%M%S'`
+NOW=$(date +'%Y%m%d-%H%M%S')
 mkdir -p ${WERCKER_CMAKE_CPACK_CMAKE_BUILD_DIR}
 cd ${WERCKER_CMAKE_CPACK_CMAKE_BUILD_DIR}
-cmake ${WERCKER_CMAKE_CPACK_CMAKE_ARGUMENTS} -DCPACK_PACKAGE_REVISION=NOW ${WERCKER_CMAKE_CPACK_CMAKE_SOURCE_PATH}
+cmake ${WERCKER_CMAKE_CPACK_CMAKE_ARGUMENTS} -DCPACK_PACKAGE_REVISION=${NOW} ${WERCKER_CMAKE_CPACK_CMAKE_SOURCE_PATH}
 make package
