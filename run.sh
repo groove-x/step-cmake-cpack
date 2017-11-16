262ab0ca0dd2127e0abac659cfea068cca01cbf2
Yuma.M <yuma-m@users.noreply.github.com>
2017-11-16 14:41:29 +0900

    Use set -eu to make script safer

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-set -e
+set -eu
 
 apt-get install -y devscripts fakeroot dpkg-dev
 
-if [ $WERCKER_CMAKE_CPACK_CLEAR_BUILD_DIR = "true" ]; then
+if [ "$WERCKER_CMAKE_CPACK_CLEAR_BUILD_DIR" = "true" ]; then
   if [ -e ${WERCKER_CMAKE_CPACK_CMAKE_BUILD_DIR} ]; then
     rm -rf ${WERCKER_CMAKE_CPACK_CMAKE_BUILD_DIR}
   fi
