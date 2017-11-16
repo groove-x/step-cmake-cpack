9342b4166fcea91cb6f7067ed4745cb5cbec65a1
Yuma.M <yuma-m@users.noreply.github.com>
2017-11-16 15:36:03 +0900

    Fix clear_build_dir if statement

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -eu
 
 apt-get install -y devscripts fakeroot dpkg-dev
 
-if [ "$WERCKER_CMAKE_CPACK_CLEAR_BUILD_DIR" = "true" ]; then
+if [ "$WERCKER_CMAKE_CPACK_CLEAR_BUILD_DIR" != "false" ]; then
   if [ -e ${WERCKER_CMAKE_CPACK_CMAKE_BUILD_DIR} ]; then
     rm -rf ${WERCKER_CMAKE_CPACK_CMAKE_BUILD_DIR}
   fi
